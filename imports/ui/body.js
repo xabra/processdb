bdf72c12410818c1eac6d906777655bb342d3bd8
xabra <adam.brailove@gmail.com>
2016-04-17 08:51:03 -0700

    Initial Commit

---
+++ imports/ui/body.js
@@ -0,0 +1,40 @@
+import { Template } from 'meteor/templating';
+
+import { Lots } from '../api/lots.js';
+
+import './body.html';
+
+Template.body.helpers({
+  lots() {
+    return Lots.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-lot'(event) {
+    // Prevent default browser form submit
+    //event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const lotID = target.lotID.value;
+    const count = target.count.value;
+    const cellType = target.cellType.value;
+
+    // Insert a new lot into the collection
+    Lots.insert({
+      lotID,
+      count,
+      cellType,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+
+    target. lotID.value = '';
+    target.count.value = '';
+    target.cellType.value = '';
+
+    return false;    // Prevent screen clear
+  },
+});
